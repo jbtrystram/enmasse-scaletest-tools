57ade58b505425602ca0873c85ed551908ae9de9
Jean-Baptiste Trystram <jbtrystram@redhat.com>
2019-11-28 17:28:22 +0100

    rework writer

---
+++ http-reader/src/main/java/net/trystram/scaletest/httpReader/Config.java
@@ -0,0 +1,137 @@
+package net.trystram.scaletest.httpReader;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import io.glutamate.lang.Environment;
+import io.glutamate.lang.Exceptions;
+import java.util.Arrays;
+import java.util.List;
+import okhttp3.HttpUrl;
+
+public class Config {
+
+    private long maxDevicesCreated = Long.MAX_VALUE;
+    private long devicesToRead = Long.MAX_VALUE;
+    private List<String> deviceIdPrefixes;
+
+    private String tenantId;
+    private String authToken;
+    private HttpUrl registryUrl;
+    private boolean insecureTls;
+    private boolean onlyRegister;
+
+    public void setMaxDevicesCreated(long maxDevicesCreated) {
+        this.maxDevicesCreated = maxDevicesCreated;
+    }
+
+    public long getMaxDevicesCreated() {
+        return maxDevicesCreated;
+    }
+
+    public void setTenantId(String tenantId) {
+        this.tenantId = tenantId;
+    }
+
+    public String getTenantId() {
+        return tenantId;
+    }
+
+    public void setDeviceIdPrefixes(List deviceIdPrefixes) {
+        this.deviceIdPrefixes = deviceIdPrefixes;
+    }
+
+    public List<String> getDeviceIdPrefixes() {
+        return deviceIdPrefixes;
+    }
+
+    public long getDevicesToRead() {
+        return devicesToRead;
+    }
+
+    public void setDevicesToRead(long devicesToRead) {
+        this.devicesToRead = devicesToRead;
+    }
+
+
+    public void setAuthToken(String authToken) {
+        this.authToken = authToken;
+    }
+
+    public String getAuthToken() {
+        return authToken;
+    }
+
+    public void setRegistryUrl(HttpUrl registryUrl) {
+        this.registryUrl = registryUrl;
+    }
+
+    public HttpUrl getRegistryUrl() {
+        return registryUrl;
+    }
+
+    public void setInsecureTls(boolean disableTls) {
+        this.insecureTls = disableTls;
+    }
+
+    public boolean isInsecureTls() {
+        return insecureTls;
+    }
+
+    public void setOnlyRegister(boolean onlyRegister) {
+        this.onlyRegister = onlyRegister;
+    }
+
+    public boolean isOnlyRegister() {
+        return onlyRegister;
+    }
+
+    public static Config fromEnv() throws IOException {
+        final Config result = new Config();
+
+        result.setAuthToken(Environment.get("AUTH_TOKEN")
+                .orElseGet(() -> Exceptions.wrap(
+                        () -> Files.readString(
+                                Paths.get("/run/secrets/kubernetes.io/serviceaccount/token"),
+                                StandardCharsets.UTF_8))));
+
+        System.out.format("Using authToken: '%s'%n", result.getAuthToken());
+
+        final String commaSeparatedStr = Environment.get("DEVICE_ID_PREFIXES").orElse("");
+        result.setDeviceIdPrefixes(Arrays.asList(commaSeparatedStr.split("\\s*,\\s*")));
+
+
+        result.setTenantId(Environment.get("TENANT_ID")
+                .or(() -> {
+                    return Environment.get("NAMESPACE")
+                            .flatMap(ns -> Environment.get("IOT_PROJECT")
+                                    .map(prj -> ns + "." + prj));
+                })
+                .orElseThrow(
+                        () -> new IllegalStateException("Missing tenant information. Need 'TENANT_ID' or 'IOT_PROJECT' and 'NAMESPACE'")));
+
+        Environment.consumeAs("MAX_DEVICES_CREATED", Long::parseLong, result::setMaxDevicesCreated);
+
+        Environment.consumeAs("DEVICES_TO_READ", Long::parseLong, result::setDevicesToRead);
+
+        result.setRegistryUrl(Environment.get("REGISTRY_URL")
+                .map(HttpUrl::parse)
+                .orElseGet(() -> {
+                    final HttpUrl.Builder builder = new HttpUrl.Builder()
+                            .scheme(Environment.get("REGISTRY_SCHEME").orElse("https"))
+                            .host(Environment.getRequired("REGISTRY_HOST"));
+
+                    Environment.consumeAs("REGISTRY_PORT", Integer::parseInt, builder::port);
+                    Environment.get("REGISTRY_BASE").ifPresent(builder::addPathSegments);
+
+                    return builder.build();
+                }));
+
+        Environment.consumeAs("INSECURE_TLS", Boolean::parseBoolean, result::setInsecureTls);
+        Environment.consumeAs("ONLY_REGISTER", Boolean::parseBoolean, result::setOnlyRegister);
+
+        return result;
+    }
+
+}
---
+++ http-reader/src/main/java/net/trystram/scaletest/httpReader/Statistics.java
@@ -0,0 +1,94 @@
+package net.trystram.scaletest.httpReader;
+
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.time.Duration;
+import java.time.Instant;
+import static java.time.Instant.now;
+import static java.time.ZoneOffset.UTC;
+import static java.time.format.DateTimeFormatter.ISO_DATE_TIME;
+import java.util.Optional;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class Statistics implements AutoCloseable {
+
+    private long success;
+    private long errorRegister;
+    private long errorCredentials;
+
+    private long timeRegister;
+    private long timeCredentials;
+
+    private ScheduledExecutorService executor;
+    private PrintStream out;
+
+    private Instant last = Instant.now();
+    private long lastSuccess;
+
+    public Statistics(final PrintStream out, final Duration print) {
+        this.out = out;
+        this.executor = Executors.newScheduledThreadPool(1);
+        this.executor.scheduleAtFixedRate(this::tick, print.toMillis(), print.toMillis(), TimeUnit.MILLISECONDS);
+        this.out.println("Time;Total;Read;Rate;ErrorsR;ErrorsC;AvgR;AvgC");
+    }
+
+    public Statistics(final OutputStream out, final Duration print) {
+        this(new PrintStream(out), print);
+    }
+
+    @Override
+    public void close() throws Exception {
+        this.executor.shutdown();
+        this.out.close();
+    }
+
+    private synchronized void tick() {
+
+        try {
+            final long currentSuccess = this.success;
+            final long diff = currentSuccess - this.lastSuccess;
+            this.lastSuccess = currentSuccess;
+
+            final Instant now = now();
+            final Duration period = Duration.between(this.last, now);
+            this.last = now;
+
+            final double rate = ((double) diff) / ((double) period.toMillis()) * 1000.0;
+
+            final Long avgReg = this.timeRegister > 0 ? this.timeRegister / diff : null;
+            final Long avgCred = this.timeCredentials > 0 ? this.timeCredentials / diff : null;
+            this.timeRegister = 0L;
+            this.timeCredentials = 0L;
+
+            this.out.format("\"%s\";%s;%s;%.2f;%s;%s;%s;%s%n",
+                    now().atZone(UTC).format(ISO_DATE_TIME),
+                    diff,
+                    currentSuccess,
+                    rate,
+                    this.errorRegister, this.errorCredentials,
+                    avgReg, avgCred);
+            this.out.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized void success(final Duration register, final Optional<Duration> credentials) {
+        this.success++;
+        this.timeRegister += register.toMillis();
+        credentials.ifPresent(c -> {
+            this.timeCredentials += c.toMillis();
+        });
+    }
+
+    public synchronized void errorRegister() {
+        this.errorRegister++;
+    }
+
+    public synchronized void errorCredentials() {
+        this.errorCredentials++;
+    }
+
+}
