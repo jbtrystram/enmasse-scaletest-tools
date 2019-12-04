3b8e5ba3300f6b8fe0990b1be0e54fba53080a75
Jens Reimann <jreimann@redhat.com>
2019-12-04 17:36:25 +0100

    Add infinispan reporter

---
+++ infinispan-reporter/src/main/java/net/trystram/scaletest/infinispan/Infinispan.java
@@ -0,0 +1,20 @@
+package net.trystram.scaletest.infinispan;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+@Path("/rest/v2")
+@RegisterRestClient
+public interface Infinispan {
+
+    @GET
+    @Path("/cache/{name}?action=size")
+    @Produces("text/plain")
+    public long cacheSize(@PathParam String name);
+
+}
