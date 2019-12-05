dff9c81e9c8848a5dbe26a00a591865d0936f555
Jens Reimann <jreimann@redhat.com>
2019-12-05 13:44:52 +0100

    try fix authentication

--- infinispan-reporter/src/main/java/net/trystram/scaletest/infinispan/Infinispan.java
+++ infinispan-reporter/src/main/java/net/trystram/scaletest/infinispan/Infinispan.java
@@ -1,15 +1,19 @@
 package net.trystram.scaletest.infinispan;
 
+import java.util.Base64;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
-
+import org.eclipse.microprofile.config.ConfigProvider;
+import org.eclipse.microprofile.rest.client.annotation.ClientHeaderParam;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 @Path("/rest/v2")
 @RegisterRestClient
+@ClientHeaderParam(name = "Authorization", value = "{lookupAuth}")
 public interface Infinispan {
 
     @GET
@@ -17,4 +21,8 @@ public interface Infinispan {
     @Produces("text/plain")
     public long cacheSize(@PathParam String name);
 
+    default String lookupAuth() {
+        final String credentials = ConfigProvider.getConfig().getValue("infinispan.credentials", String.class);
+        return "Basic " + Base64.getEncoder().encodeToString(credentials.getBytes());
+      }
 }
