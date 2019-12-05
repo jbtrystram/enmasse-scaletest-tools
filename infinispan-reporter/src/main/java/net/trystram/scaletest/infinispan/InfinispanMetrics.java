7743c4d77ef5b113fdf5cc76e475bd5cca26abe4
Jens Reimann <jreimann@redhat.com>
2019-12-05 13:20:31 +0100

    Try work around missing metrics

--- infinispan-reporter/src/main/java/net/trystram/scaletest/infinispan/InfinispanMetrics.java
+++ infinispan-reporter/src/main/java/net/trystram/scaletest/infinispan/InfinispanMetrics.java
@@ -1,13 +1,19 @@
 package net.trystram.scaletest.infinispan;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 import org.eclipse.microprofile.metrics.MetricUnits;
 import org.eclipse.microprofile.metrics.annotation.Gauge;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.quarkus.runtime.StartupEvent;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 
 @ApplicationScoped
@@ -15,12 +21,19 @@ import io.quarkus.runtime.annotations.RegisterForReflection;
 @Path("/info")
 public class InfinispanMetrics {
 
+    private static final Logger logger = LoggerFactory.getLogger(InfinispanMetrics.class);
+
     @Inject
     @RestClient
     Infinispan infinispan;
 
+    void onStart(@Observes StartupEvent ev) {
+        logger.info("The application is starting...");
+    }
+
     @GET
     @Path("/devices")
+    @Produces("text/plain")
     public Long devices() {
         return this.infinispan.cacheSize("devices");
     }
