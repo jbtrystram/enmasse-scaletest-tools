496832a0c0db821807bbef18dbaad8b9098a2ac9
Jens Reimann <jreimann@redhat.com>
2019-12-05 11:26:26 +0100

    Try working around missing constructor issue

--- infinispan-reporter/src/main/java/net/trystram/scaletest/infinispan/InfinispanMetrics.java
+++ infinispan-reporter/src/main/java/net/trystram/scaletest/infinispan/InfinispanMetrics.java
@@ -6,25 +6,19 @@ import javax.inject.Inject;
 import org.eclipse.microprofile.metrics.MetricUnits;
 import org.eclipse.microprofile.metrics.annotation.Gauge;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
-
-import io.quarkus.scheduler.Scheduled;
+import io.quarkus.runtime.annotations.RegisterForReflection;
 
 @ApplicationScoped
+@RegisterForReflection
 public class InfinispanMetrics {
 
     @Inject
     @RestClient
     Infinispan infinispan;
-    private long cacheEntries;
 
     @Gauge(name = "cache_entries", tags = {"cache=devices"}, unit = MetricUnits.NONE, description = "Total number of entries in the 'devices' cache")
     public Long cacheEntries() {
-        return this.cacheEntries;
-    }
-
-    @Scheduled(every = "10s")
-    public void update() {
-        this.cacheEntries = this.infinispan.cacheSize("devices");
+        return this.infinispan.cacheSize("devices");
     }
 
 }
