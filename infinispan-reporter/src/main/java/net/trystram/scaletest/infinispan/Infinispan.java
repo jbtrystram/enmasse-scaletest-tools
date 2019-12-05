cd0a720b68906c47bae6883bf763305b11728c91
Jens Reimann <jreimann@redhat.com>
2019-12-05 14:03:02 +0100

    Try working around auth issue

--- infinispan-reporter/src/main/java/net/trystram/scaletest/infinispan/Infinispan.java
+++ infinispan-reporter/src/main/java/net/trystram/scaletest/infinispan/Infinispan.java
@@ -1,28 +1,19 @@
 package net.trystram.scaletest.infinispan;
 
-import java.util.Base64;
-
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
-import org.eclipse.microprofile.config.ConfigProvider;
-import org.eclipse.microprofile.rest.client.annotation.ClientHeaderParam;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 @Path("/rest/v2")
 @RegisterRestClient
-@ClientHeaderParam(name = "Authorization", value = "{lookupAuth}")
 public interface Infinispan {
 
     @GET
     @Path("/caches/{name}?action=size")
     @Produces("text/plain")
-    public long cacheSize(@PathParam String name);
-
-    default String lookupAuth() {
-        final String credentials = ConfigProvider.getConfig().getValue("infinispan.credentials", String.class);
-        return "Basic " + Base64.getEncoder().encodeToString(credentials.getBytes());
-      }
+    public long cacheSize(@HeaderParam("Authorization") String authHeader, @PathParam String name);
 }
--- infinispan-reporter/src/main/java/net/trystram/scaletest/infinispan/InfinispanMetrics.java
+++ infinispan-reporter/src/main/java/net/trystram/scaletest/infinispan/InfinispanMetrics.java
@@ -1,5 +1,7 @@
 package net.trystram.scaletest.infinispan;
 
+import java.util.Base64;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
@@ -7,6 +9,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
+import org.eclipse.microprofile.config.ConfigProvider;
 import org.eclipse.microprofile.metrics.MetricUnits;
 import org.eclipse.microprofile.metrics.annotation.Gauge;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
@@ -27,20 +30,24 @@ public class InfinispanMetrics {
     @RestClient
     Infinispan infinispan;
 
+    private String authHeader;
+
     void onStart(@Observes StartupEvent ev) {
         logger.info("The application is starting...");
+        final String credentials = ConfigProvider.getConfig().getValue("infinispan.credentials", String.class);
+        this.authHeader = "Basic " + Base64.getEncoder().encodeToString(credentials.getBytes());
     }
 
     @GET
     @Path("/devices")
     @Produces("text/plain")
     public Long devices() {
-        return this.infinispan.cacheSize("devices");
+        return this.infinispan.cacheSize(this.authHeader, "devices");
     }
 
     @Gauge(name = "cache_entries", tags = {"cache=devices"}, unit = MetricUnits.NONE, description = "Total number of entries in the 'devices' cache")
     public Long cacheEntries() {
-        return this.infinispan.cacheSize("devices");
+        return this.infinispan.cacheSize(this.authHeader, "devices");
     }
 
 }
